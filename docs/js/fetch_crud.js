28ddbcd4a0f7e4404af86f49c86cc068cd4b34d7
Ryo Iida <iidazz0109@gmail.com>
2020-08-11 08:29:19 +0900

    DOM操作追記、Read設定追加

--- docs/js/fetch_crud.js
+++ docs/js/fetch_crud.js
@@ -1,10 +1,17 @@
 const postIkku = document.querySelector('.postIkku');
 const postBtn = document.querySelector('.postBtn');
+const ikkulist = document.querySelector('.ikkuDl');
 const url = 'http://localhost:3000/ikku';
 
+const appendList = (thisData) => {
+    const li = document.createElement("li");
+    li.dataset.id = thisData.id;
+    li.innerHTML = thisData.ikku;
+    ikkulist.appendChild(li);    
+};
 
 // Create
-const postFetch = () => {
+const createFetch = () => {
     const data = {
         ikku: postIkku.value
     };
@@ -18,13 +25,33 @@ const postFetch = () => {
         if(!response.ok) {
             console.log('error!');
         } 
-        console.log('ok!: ' + response);
+        console.log('ok!');
         return response.json();
     }).then((data)  => {
-        console.log(data);
+        appendList(data);
     }).catch((error) => {
         console.log(error);
     });
 };
 
-postBtn.addEventListener('click', postFetch, false);
+postBtn.addEventListener('click', createFetch, false);
+
+
+// Read
+const readFetch = () => {
+    fetch(url).then((response) => {
+        if(!response.ok) {
+            console.log('error!');
+        } 
+        console.log('ok!');
+        return response.json();
+    }).then((data)  => {
+        for (let i = 0; i < data.length; i++) {
+            const thisData = data[i];
+            appendList(thisData);
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+};
+readFetch();
